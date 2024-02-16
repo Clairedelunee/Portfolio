5615d0282bf1547ac66df245ca7a002a02ddf6a2
Clairedelunee <115539104+Clairedelunee@users.noreply.github.com>
2024-02-16 17:36:32 +0100

    repair header final?

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript (script.js)
-function ajouterClasseSiSurPage(elementId, pagePartielle, classe) {
-    if (window.location.href.indexOf(pagePartielle) > -1) {
-        var element = document.getElementById(elementId);
-        if (element) {
-            element.classList.add(classe);
-        }
-    }
-}
