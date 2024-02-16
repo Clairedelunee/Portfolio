ce5db1bb9cb780e291abda15fae076f0798d60ec
Clairedelunee <115539104+Clairedelunee@users.noreply.github.com>
2024-02-16 17:28:30 +0100

    test js?

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-
+// JavaScript (script.js)
 function ajouterClasseSiSurPage(elementId, pagePartielle, classe) {
-    // Vérifie si l'utilisateur est sur la page spécifique
     if (window.location.href.indexOf(pagePartielle) > -1) {
-        // Récupère l'élément par son ID
         var element = document.getElementById(elementId);
-
-        // Ajoute la classe à l'élément
         if (element) {
             element.classList.add(classe);
         }
     }
 }
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Utilisation de la fonction pour ajouter la classe sur différentes pages
-    ajouterClasseSiSurPage("indexpage", "index.html", "portfolioactif");
-    ajouterClasseSiSurPage("pagecontact", "contact.html", "contactactive");
-    // Ajoutez autant d'appels que nécessaire pour les différentes pages
-});
